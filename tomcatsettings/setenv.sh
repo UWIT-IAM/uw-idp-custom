a8b05b67be4f9c8340ecfae8f29c6f15e8724d07
Andrew Markiel <markiel@uw.edu>
2026-06-17 19:23:53 -0700

    Adding our custom Tomcat configuration to source control.

---
+++ tomcatsettings/setenv.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Environment variables for Tomcat, linked from ${TOMCAT_HOME}/bin/setenv.sh
+#
+# The CATALINA_OPTS are things used by Tomcat only. Mostly Java memory options,
+# but note the essential logging configuration that controls where the logs are stored.
+#
+# The JAVA_OPTS variable sets the location of the IdP configuration files; this is
+# necessary, otherwise the Shibboleth webapp will expect them in /opt/shibboleth-idp.
+# This has to be in JAVA_OPTS since it is used by the webapp, not Tomcat itself.
+#
+# The CATALINA_OUT variable sets the location of the catalina.out file, which is
+# the Tomcat output written to stdout/stderr. This is output from Tomcat itself
+# rather than from any of the individual webapps.
+
+export CATALINA_OPTS=-server -XX:+UseParallelGC -Xss24m -Xms4g -Xmx4g -XX:MaxGCPauseMillis=400 -Dlog4j.configuration=file:/usr/local/tomcat/conf/log4j.properties
+export JAVA_OPTS=-Didp.home=/data/local/idp
+export CATALINA_OUT=/data/logs/tomcat/catalina.out
